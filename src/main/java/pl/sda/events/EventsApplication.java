343935fc5de217ddbad31d3f29fce078c67b6b2c
RafalDrgas <rafinho91@gmail.com>
2018-07-13 17:52:47 +0200

    first commit

---
+++ src/main/java/pl/sda/events/EventsApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApplication.class, args);
+	}
+}
